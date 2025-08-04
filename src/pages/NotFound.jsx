354f3c4463d3057863598467dc5c35787840df43
sandipan bisaws <biswassandipan058@gmail.com>
2025-08-05 03:22:46 +0530

    frist massege

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='bg-gray-300 text-red-300 w-200'>
+      <h1 className='w-300 l-200 '>ERROR 404: PAGE NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default NotFound
